1bcde56914bbd8cc2e97e814871af2cfe5650c0b
Abhishek Jadaun <am3239@srmist.edu.in>
2024-01-09 15:07:08 +0530

    Initial Commit

---
+++ dermShieldModelTesting/dermShieldModelTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  dermShieldModelTestingApp.swift
+//  dermShieldModelTesting
+//
+//  Created by Abhishek Jadaun on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct dermShieldModelTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
